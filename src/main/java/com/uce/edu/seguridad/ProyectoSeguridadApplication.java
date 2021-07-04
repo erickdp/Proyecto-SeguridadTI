f4ec20871b72faf9e97ff54a231c08f2c9e12b1e
Erick Díaz <erickdp@hotmail.com>
2021-07-04 17:55:25 -0500

    Initial commit

---
+++ src/main/java/com/uce/edu/seguridad/ProyectoSeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.seguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSeguridadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoSeguridadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uce/edu/seguridad/ProyectoSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSeguridadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
