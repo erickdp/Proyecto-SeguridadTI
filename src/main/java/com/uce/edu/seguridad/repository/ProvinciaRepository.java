92b1e288ea76a15811de66a737aaf3af55fa2303
Erick Díaz <erickdp@hotmail.com>
2021-07-13 15:10:12 -0500

    Deploy sin provincia

--- src/main/java/com/uce/edu/seguridad/repository/ProvinciaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uce.edu.seguridad.repository;
-
-import com.uce.edu.seguridad.models.Provincia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProvinciaRepository extends JpaRepository<Provincia, Long> {
-}
--- src/main/java/com/uce/edu/seguridad/service/ProvinciaService.java
+++
@@ -1,6 +0,0 @@
-package com.uce.edu.seguridad.service;
-
-import com.uce.edu.seguridad.models.Provincia;
-
-public interface ProvinciaService extends BaseService<Provincia> {
-}
